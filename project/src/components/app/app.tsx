236226ad40ed98ba8427c0cc0b8a662b1e3f5f9a
capykasa <capykasa@gmail.com>
2021-10-05 17:46:58 +0500

    Создал приватный маршрут к Favorites

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../main-screen/main-screen';
 import LoginScreen from '../login-screen/login-screen';
 import FavoritesScreen from '../favorites-screen/favorites-screen';
 import PropertyScreen from '../property-screen/property-screen';
 import PageNotFound from '../page-not-found/page-not-found';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   placesCount: number;
@@ -22,9 +23,13 @@ function App({ placesCount }: AppScreenProps): JSX.Element {
         <Route exact path={AppRoute.Login}>
           <LoginScreen />
         </Route>
-        <Route exact path={AppRoute.Favorites}>
-          <FavoritesScreen />
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <FavoritesScreen />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
         <Route exact path={AppRoute.Room}>
           <PropertyScreen />
         </Route>
