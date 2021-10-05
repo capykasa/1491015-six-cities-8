ca922c67b43ece60ac68141b944f7c01800a24c3
capykasa <capykasa@gmail.com>
2021-10-05 17:18:50 +0500

    Добавил маршрутизацию и изменил a на link у логотипа

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../main-screen/main-screen';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import PropertyScreen from '../property-screen/property-screen';
+import PageNotFound from '../page-not-found/page-not-found';
 
 type AppScreenProps = {
   placesCount: number;
@@ -6,7 +12,27 @@ type AppScreenProps = {
 
 function App({ placesCount }: AppScreenProps): JSX.Element {
   return (
-    <MainScreen placesCount={placesCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainScreen
+            placesCount={placesCount}
+          />
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <LoginScreen />
+        </Route>
+        <Route exact path={AppRoute.Favorites}>
+          <FavoritesScreen />
+        </Route>
+        <Route exact path={AppRoute.Room}>
+          <PropertyScreen />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -1,3 +1,4 @@
+import Logo from '../logo/logo';
 import PlaceCard from '../place-card/place-card';
 
 const url = '';
@@ -16,9 +17,7 @@ function MainScreen({ placesCount }: MainScreenProps): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active" href={url}>
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
