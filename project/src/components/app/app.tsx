7db8a938b6cab699a569a20e126defb55aab9d34
capykasa <capykasa@gmail.com>
2021-10-15 18:17:01 +0500

    Подключил моки в App и index. Не используются

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -6,12 +6,14 @@ import FavoritesScreen from '../favorites-screen/favorites-screen';
 import PropertyScreen from '../property-screen/property-screen';
 import PageNotFound from '../page-not-found/page-not-found';
 import PrivateRoute from '../private-route/private-route';
+import { Offers } from '../../types/offers';
 
 type AppScreenProps = {
   placesCount: number;
+  offers: Offers[];
 }
 
-function App({ placesCount }: AppScreenProps): JSX.Element {
+function App({ placesCount, offers }: AppScreenProps): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import { offers } from './mocks/offers';
 
 const CARD_COUNT = 5;
 
@@ -8,6 +9,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App
       placesCount={CARD_COUNT}
+      offers={offers}
     />
   </React.StrictMode>,
   document.getElementById('root'));
