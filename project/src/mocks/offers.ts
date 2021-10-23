80f13ff0028498d9b1fb6d17752946641df3c55b
capykasa <capykasa@gmail.com>
2021-10-23 15:03:10 +0500

    Подключил leaflet (возможно) и добавил координаты к мокам

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -24,8 +24,8 @@ export const offers: Offer[] = [
     isFavorite: false,
     isPremium: false,
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
       zoom: 8,
     },
     maxAdults: 4,
@@ -39,8 +39,8 @@ export const offers: Offer[] = [
     bedrooms: 1,
     city: {
       location: {
-        latitude: 50.370216,
-        longitude: 4.795168,
+        latitude: 52.370216,
+        longitude: 4.895168,
         zoom: 10,
       },
       name: 'Amsterdam',
@@ -58,8 +58,8 @@ export const offers: Offer[] = [
     isFavorite: true,
     isPremium: true,
     location: {
-      latitude: 50.35514938496378,
-      longitude: 4.273877537499948,
+      latitude: 52.369553943508,
+      longitude: 4.85309666406198,
       zoom: 8,
     },
     maxAdults: 14,
@@ -73,8 +73,8 @@ export const offers: Offer[] = [
     bedrooms: 5,
     city: {
       location: {
-        latitude: 56.370216,
-        longitude: 5.895168,
+        latitude: 52.370216,
+        longitude: 4.895168,
         zoom: 10,
       },
       name: 'Brussels',
@@ -92,8 +92,8 @@ export const offers: Offer[] = [
     isFavorite: true,
     isPremium: false,
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
       zoom: 8,
     },
     maxAdults: 8,
@@ -126,8 +126,8 @@ export const offers: Offer[] = [
     isFavorite: true,
     isPremium: false,
     location: {
-      latitude: 55.35514938496378,
-      longitude: 2.673877537499948,
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
       zoom: 8,
     },
     maxAdults: 2,
